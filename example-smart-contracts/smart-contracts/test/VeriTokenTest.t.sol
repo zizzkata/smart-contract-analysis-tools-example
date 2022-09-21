450419fcb7ba650c1b0acca81792023a8083bd15
enzo evers <enzoevers@hotmail.com>
2022-09-21 20:17:51 +0200

    Update readme

--- example-smart-contracts/smart-contracts/test/VeriTokenTest.t.sol
+++ example-smart-contracts/smart-contracts/test/VeriTokenTest.t.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "Utils/Utils.sol";
+import "src/VeriToken.sol";
 
 contract VeriTokenTest is Test {
+    Utils utils;
+    VeriToken token;
 
     function setUp() public {
+        // vm comes from Test -> Script
+        utils = new Utils(vm);
+        token = new VeriToken();
     }
 }
