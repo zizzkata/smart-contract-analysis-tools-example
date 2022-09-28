e7272a55129b2ee872e6a9dce8aa44df2fbeee5b
enzoevers <enzoeers@hotmail.com>
2022-09-28 11:35:57 +0000

    Trying docker buildx

---
+++ releaseDockerKevm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "================================================================="
+echo "Build and push Dockerfile.kevm"
+echo "================================================================="
+echo ""
+
+docker buildx  build --platform=linux/amd64,linux/arm64/v8 --pull -t ghcr.io/byont-ventures/kevm:latest -f Dockerfile.kevm  .
+docker push ghcr.io/byont-ventures/kevm:latest
