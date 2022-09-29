d22c18f5397693b7e344a94ce17e5649d7ebcbd5
Enzo Evers <enzoevers@hotmail.com>
2022-09-29 08:25:41 +0200

    Added docker buildx create --use to the docker script

--- docker/releaseDockerKevm.sh
+++ docker/releaseDockerKevm.sh
@@ -6,5 +6,6 @@ echo "Build and push Dockerfile.kevm"
 echo "================================================================="
 echo ""
 
-docker buildx  build --platform=linux/amd64,linux/arm64/v8 --pull -t ghcr.io/byont-ventures/kevm:latest -f Dockerfile.kevm  .
+docker buildx create --use
+docker buildx build --platform=linux/amd64,linux/arm64/v8 --pull -t ghcr.io/byont-ventures/kevm:latest -f Dockerfile.kevm  .
 docker push ghcr.io/byont-ventures/kevm:latest
