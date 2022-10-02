c9293c1f719d0fea51181d89d6544b70bc65b93b
enzoevers <enzoeers@hotmail.com>
2022-10-02 11:37:28 +0000

    Create release script for the hevm docker

--- docker/releaseDockerHevm.sh
+++ docker/releaseDockerHevm.sh
@@ -2,9 +2,9 @@
 
 echo ""
 echo "================================================================="
-echo "Build and push Dockerfile.kevm"
+echo "Build and push Dockerfile.hevm"
 echo "================================================================="
 echo ""
 
 docker buildx create --use
-docker buildx build --push --platform=linux/amd64 --pull -t ghcr.io/byont-ventures/kevm:latest -f Dockerfile.kevm  .
+docker buildx build --push --platform=linux/amd64 --pull -t ghcr.io/byont-ventures/hevm:latest -f Dockerfile.hevm  .
