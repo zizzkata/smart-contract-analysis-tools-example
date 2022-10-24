15bd20f25971e966b14eefcca9cd6e9fb866f426
enzoevers <enzoeers@hotmail.com>
2022-10-24 09:22:29 +0000

    Creating an acution in both ways ETH <-> ERC20 for different exploits

--- example-smart-contracts/smart-contracts/src/WithProblems/VeriAuctionTokenForEth.sol
+++ example-smart-contracts/smart-contracts/src/WithProblems/VeriAuctionTokenForEth.sol
@@ -5,7 +5,7 @@ pragma abicoder v2;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract VeriAuction is Ownable {
+contract VeriAuctionTokenForEth is Ownable {
 
     //======================================================
     // Events
@@ -38,25 +38,34 @@ contract VeriAuction is Ownable {
 
  
     function commit() payable external {
-        require(msg.value > 0, "VeriAuction (commit): commitment must be greater than 0");
+        require(msg.value > 0, "VeriAuctionTokenForEth (commit): commitment must be greater than 0");
 
         commited[msg.sender] += msg.value;
         
         emit Commited(msg.sender, msg.value, this.balance, getCurrentPrice());
     }
-
+    
+    /// @notice Calculates the price of one whole auction token in eth.
+    /// @dev The use of this.balance make for a possible exploit that can increase the
+    ///      price of an auction token.
     function getCurrentPrice() view public returns (uint256 pricePerAuctionToken) {
-        pricePerAuctionToken = (this.balance * 10**auctionToken.decimals()) / (_amountToDistribute * 1e18);
+        pricePerAuctionToken = (this.balance * 10**auctionToken.decimals()) / _amountToDistribute;
     }
 
     function finalize() public onlyOwner {
         auctionFinalized = true;
     }
 
-
+    
+    /// @dev The use of this.balance make for a possible exploit that can increase the
+    ///      price of an auction token.
+    ///      
     function claim(uint256 amount) external {
-       require(auctionFinalized, "VeriAuction (claim): Auction not finalized yet");
+        require(auctionFinalized, "VeriAuctionTokenForEth (claim): Auction not finalized yet");
+        
+        uint256 share = (commited[msg.sender] * 1e18) / this.balance;
+        uint256 claimable = (share * _amountToDistribute) / 1e18;
 
-       
+        auctionToken.transfer(msg.sender, claimable)
     }
 }
