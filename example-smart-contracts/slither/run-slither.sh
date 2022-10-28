2c6955449a24830b950ad17fd58fb0da95635969
enzoevers <enzoeers@hotmail.com>
2022-10-28 13:37:34 +0000

    Made slither work

--- example-smart-contracts/slither/run-slither.sh
+++ example-smart-contracts/slither/run-slither.sh
@@ -5,22 +5,16 @@ contractName=$2
 
 echo ""
 echo "================================================================="
-echo "Flatten the contract to be verified"
-echo "================================================================="
-echo ""
-
-docker run --rm -v ${projectRoot}:/prj ghcr.io/foundry-rs/foundry:latest "  \
-    cd /prj/smart-contracts                                                 \
-    && forge flatten src/${contractName}.sol                                \
-    --output ../flattened/${contractName}-flat.sol"
-
-echo ""
-echo "================================================================="
 echo "Run Slither"
 echo "================================================================="
 echo ""
 
-docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/analysis-tools:latest /bin/bash -c "             \
-     slither --json ${contractName}-output.json                                                             \
-     --config-file /prj/slither/slither.config.json                                                         \
-     ${contractName} 2>&1 | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | tee ${contractName}-console-output.log"
+cp slither.config.json ${projectRoot}/smart-contracts/slither.config.json
+
+docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/analysis-tools:latest /bin/bash -c "  \
+    cd /prj/smart-contracts &&                              \
+    slither --json /prj/slither/${contractName}-output.json \
+    --config-file /prj/smart-contracts/slither.config.json  \
+    /prj/smart-contracts/src/${contractName}.sol"
+
+rm ${projectRoot}/smart-contracts/slither.config.json
