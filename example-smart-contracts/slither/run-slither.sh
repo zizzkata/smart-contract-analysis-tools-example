2daedfbcd1d2101bac36fe2799b221e603f138b2
enzoevers <enzoeers@hotmail.com>
2022-10-28 18:34:45 +0000

    Update slither.confic.json

--- example-smart-contracts/slither/run-slither.sh
+++ example-smart-contracts/slither/run-slither.sh
@@ -9,12 +9,9 @@ echo "Run Slither"
 echo "================================================================="
 echo ""
 
-cp slither.config.json ${projectRoot}/smart-contracts/slither.config.json
-
 docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/analysis-tools:latest /bin/bash -c "  \
-    cd /prj/smart-contracts &&                              \
-    slither --json /prj/slither/${contractName}-output.json \
-    --config-file /prj/smart-contracts/slither.config.json  \
+    cd /prj/slither/                                            \
+    && rm -f /prj/slither/${contractName}-output.json           \
+    && slither --json /prj/slither/${contractName}-output.json  \
+    --config-file /prj/slither/slither.config.json              \
     /prj/smart-contracts/src/${contractName}.sol"
-
-rm ${projectRoot}/smart-contracts/slither.config.json
