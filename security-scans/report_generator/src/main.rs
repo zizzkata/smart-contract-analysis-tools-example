d8f1f43e8fce2c97ebe0c61d36ed8eb6e01b259f
enzoevers <enzoeers@hotmail.com>
2022-10-30 12:18:10 +0000

    Can run the slither script using rust

--- security-scans/report_generator/src/main.rs
+++ security-scans/report_generator/src/main.rs
@@ -3,10 +3,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    // TODO: show message if no argument was given
     let contract_name = &args[1];
 
     let path = env::current_dir().expect("ERROR: Failed to get current path!");
     let path_string = path.to_str().expect("ERROR: can not convert path to string!");
-    let _result = slither::run_slither(path_string, contract_name);
-    // println!("Result is {}", result);
+    let result = slither::run_slither(path_string, contract_name);
+    println!("Result: {}", result);
 }
--- security-scans/slither/slither_runner/src/lib.rs
+++ security-scans/slither/slither_runner/src/lib.rs
@@ -1,14 +1,23 @@
-//use std::process::Command;
+use std::process::Command;
+use std::str;
 
-pub fn run_slither(prj_root_path: &str, contract_name: &str) {
-    println!("prj_root_path: {}", prj_root_path);
-    println!("contract_name: {}", contract_name);
+pub fn run_slither(prj_root_path: &str, contract_name: &str) -> String {
 
-    // let result = Command::new("sh")
-    //     .arg("-c")
-    //     .arg("echo hello")
-    //     .output()
-    //     .expect("failed to execute process")
+    let command = format!("sudo ./security-scans/slither/run-slither.sh {} {}", prj_root_path, contract_name);
+
+    // ./security-scans/slither/run-slither.sh ${PWD}
+    let result = Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("failed to execute process");
+
+    let output = match str::from_utf8(&result.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    return output.to_string();
 }
 
 
